18930d8e94dd3f9a9914e0e9fe82d1d7c9c4b1f1
DeathNet123 <arslan.computer2002@gmail.com>
2023-01-10 23:28:13 +0500

    added the horners rule recursive

--- Problems/p_2_4.cpp
+++ Problems/p_2_4.cpp
@@ -14,7 +14,12 @@ int horners_iterative(vector<int> equation, int x, int power)
     return result;
 }
 
-
+int horners_recursive(vector<int> equation, int x, int idx)
+{
+    if(idx == 0)
+        return equation[idx];
+    return equation[idx] + horners_recursive(equation, x, idx - 1) * x;
+}
 
 int main(int argc, char** argv)
 {
@@ -23,9 +28,9 @@ int main(int argc, char** argv)
 
     vector<int> equation {6, -2, 7, 5}; //filling the missing powers polynomial with zero if any of the
     printf("Answer by iteration:  %d\n", horners_iterative(equation, 4, 3));
-    printf("Answer by Recursion:  %d\n", horners_recursive(equation, 4, 3, 0));
+    printf("Answer by Recursion:  %d\n", horners_recursive(equation, 4, 3));
     vector<int> equation2 {5, 0, -3, 0, 12}; //filling the missing powers polynomial with zero if any of the
     printf("Answer by iteration:%d\n", horners_iterative(equation2, -4, 4));
-    printf("Answer by Recursion:  %d\n", horners_recursive(equation, -4, 4, 0));
+    printf("Answer by Recursion:  %d\n", horners_recursive(equation, -4, 4));
     return 0;
 }
