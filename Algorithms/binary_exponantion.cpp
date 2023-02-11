c8f5cba95c08b736c434a097fba41ffb774d2604
DeathNet123 <arslan.computer2002@gmail.com>
2023-02-11 15:30:57 +0500

    Binary Exponantion added

--- Algorithms/binary_exponantion.cpp
+++ Algorithms/binary_exponantion.cpp
@@ -4,11 +4,21 @@ using namespace std;
 
 long long power(int n, int m)
 {
-    
+    long long res = 1;
+    int power = n;
+    while(m > 0)
+    {
+        if(m & 1)
+            res *= power;
+        m = m >> 1;
+        power *= power;
+    }
+    return res;
 }
 
 int main(int argc, char **argv)
 {
-
+    long long res = power(3, 13);
+    cout<< res;
     return 0;
 }
